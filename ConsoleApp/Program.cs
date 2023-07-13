a1e0bc21d944ec66b479af54fd502bcc3bd5fab5
Paweł Łęczycki <pawel@msd.info.pl>
2023-07-13 10:51:19 +0200

    Projekt startowy

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// C# wspiera przeciążanie funkcji
+// jedna nazwa może być prztpisana do wielu funkcjonalności, jeśli różnią się one parametrami
+Console.WriteLine();
+Console.WriteLine("Hello, World!");
+
+
